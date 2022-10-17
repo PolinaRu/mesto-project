a0ec70c06306190d252a2b56a04151b10b986c4a
Rubacova Polina <rubacova.polina@yandex.ru>
2022-10-17 10:41:53 +0300

    future/js for elements

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 const profile__name = document.querySelector('.profile__name');
-const profile__about = document.querySelector('.profile__about')
+const profile__about = document.querySelector('.profile__about');
+let elements = document.querySelector('.elements');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
 
 document.querySelector('.profile__edit').addEventListener('click', function() {
   document.querySelector('#name').value = profile__name.textContent;
