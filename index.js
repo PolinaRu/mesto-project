8c78b706587dd08aba9300b7871b989fd261395a
Rubacova Polina <rubacova.polina@yandex.ru>
2022-10-17 11:23:34 +0300

    js for elements

--- index.js
+++ index.js
@@ -31,11 +31,11 @@ const initialCards = [
 document.querySelector('.profile__edit').addEventListener('click', function() {
   document.querySelector('#name').value = profile__name.textContent;
   document.querySelector('#about').value = profile__about.textContent;
-  document.querySelector('.popup').classList.toggle('popup_opend');
+  document.querySelector('#editProfile').classList.toggle('popup_opend');
 })
 
 document.querySelector('.popup__close-button').addEventListener('click', function() {
-  document.querySelector('.popup').classList.toggle('popup_opend');
+  document.querySelector('#editProfile').classList.toggle('popup_opend');
 })
 
 document.querySelector('.popup__save-button').addEventListener('submit', formSubmitHandler); 
@@ -47,4 +47,19 @@ function formSubmitHandler(evt) {
   profile__about.textContent = document.querySelector('#about').value;
 
   document.querySelector('.popup').classList.toggle('popup_opend');
+}
+
+function addElement(nameEl, linkEl) {
+  elements.innerHTML += `<article class="element">
+  <div style="background-image: url(${linkEl});" alt="${nameEl}" class="element__image"></div>
+  <button class="element__drop" type="button"></button>
+  <div class="element__text">
+    <h2 class="element__name">${nameEl}</h2>
+    <button class="element__like" type="button"></button>
+  </div>
+</article>`;
+}
+
+for (let i in initialCards) {
+  addElement(initialCards[i].name, initialCards[i].link);
 }
